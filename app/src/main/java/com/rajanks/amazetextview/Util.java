7527c0c02e786396d21518d9f2ace0eefe104837
rajan ks <rajan.ks93@gmail.com>
2016-06-16 11:09:00 +0530

    Initial commit

---
+++ app/src/main/java/com/rajanks/amazetextview/Util.java
@@ -0,0 +1,11 @@
+package com.rajanks.amazetextview;
+
+import android.content.Context;
+import android.graphics.Typeface;
+
+/**
+ * Created by rajan.kali on 16/06/16.
+ */
+public class Util {
+
+}
